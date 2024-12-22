a182a3372adf1fe65578cc9c39006d8b1f285f82
Sumit Anil Anarase <115489260+sumitanarase@users.noreply.github.com>
2024-12-22 19:05:19 +0530

    mongodb.js

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,29 +1,12 @@
-import dotenv from "dotenv";
 import mongoose from "mongoose";
 
-// Load environment variables from .env file
-dotenv.config();
-
 const connectDB = async () => {
-    try {
-        mongoose.connection.on('connected', () => console.log("Database connected successfully"));
-        mongoose.connection.on('error', (err) => console.error(`Database connection error: ${err.message}`));
 
-        // Ensure the URI is defined
-        const mongoUri = process.env.MONGODB_URI;
-        if (!mongoUri) {
-            throw new Error("MONGODB_URI is not defined in the environment variables.");
-        }
+    mongoose.connection.on('connected', () => console.log("Database Connected"))
+    await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`)
 
-        // Connect to MongoDB
-        await mongoose.connect(mongoUri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-    } catch (error) {
-        console.error("Failed to connect to MongoDB:", error.message);
-        process.exit(1); // Exit process with failure
-    }
-};
+}
 
 export default connectDB;
+
+// Do not use '@' symbol in your databse user's password else it will show an error.
